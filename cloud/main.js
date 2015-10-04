495e3d57361258a25ce4c63c12677cc35cab5298
Kyle Szela <kszela24@gmail.com>
2015-10-03 20:14:13 -0500

    Added afterSave code and job to create new running tally every 30 minutes

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,29 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var tweet = Parse.Object.extend("Tweet");
+var runningTally = Parse.Object.extend("runningTallyNU")
+
+Parse.Cloud.afterSave("Tweet", function(request){
+	var query = new Parse.Query("runningTallyNU");
+	var currRunningTally = new runningTally();
+	query.ascending("createdAt");
+	query.first().then(function(queryResult){
+		currRunningTally = queryResult;
+		var countNeg = currRunningTally.get("negative");
+		var countPos = currRunningTally.get("positive");
+		if (request.object.get("classification") > 0) {
+			countPos++;
+			currRunningTally.set("positive", countPos);
+		} else {
+			countNeg++;
+			currRunningTally.set("negative", countNeg);
+		}
+		currRunningTally.save();
+	});
+});
+
+Parse.Cloud.job("newRunningTallyNU", function(request, response) {
+	var newRunningTallyNU = new runningTally()
+	newRunningTallyNU.set("positive", 0);
+	newRunningTallyNU.set("negative", 0);
+	newRunningTallyNU.save();
 });
